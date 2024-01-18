4ccebd7529e84d32d711f98a0378528ed5bab461
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-01-18 22:45:00 +0100

    feat: Welcome & home page

---
+++ App.js
@@ -0,0 +1,14 @@
+import Home from "./app/index";
+
+export default function App() {
+  return <Home />;
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });

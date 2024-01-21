7d39f2014f55eb88f78c4a9bdfd0a4d06709f98e
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-01-21 14:47:33 +0100

    full app

--- app/index.js
+++ app/index.js
@@ -1,45 +1,12 @@
-import { useState } from "react";
-import { View, Text, ScrollView, SafeAreaView } from "react-native";
-import { Stack, useRouter } from "expo-router";
+// index.js
+import { Redirect, useRouter } from "expo-router"; // Correct import
 
-import { COLORS, icons, images, SIZES } from "../constants";
-import {
-  Nearbyjobs,
-  Popularjobs,
-  ScreenHeaderBtn,
-  Welcome,
-} from "../components";
-
-const Home = () => {
+export default function Index() {
   const router = useRouter();
 
-  //   GitHub Gist
-  //   https://gist.github.com/adrianhajdin/dcc49a54d2822daae4b71b74265c397e
+  // Example of redirecting to "/home"
+  router.replace("/home");
 
-  return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
-      <Stack.Screen
-        options={{
-          headerStyle: { backgroundColor: COLORS.lightWhite },
-          headerShadowVisible: false,
-          headerLeft: () => (
-            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
-          ),
-          headerRight: () => (
-            <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
-          ),
-          headerTitle: "",
-        }}
-      />
-      <ScrollView showsVerticalScrollIndicator={false}>
-        <View style={{ flex: 1, padding: SIZES.medium }}>
-          <Welcome />
-          <Popularjobs />
-          <Nearbyjobs />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-export default Home;
+  // Return null or any placeholder since you're using redirection
+  return null;
+}
